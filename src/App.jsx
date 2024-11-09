2cca0f4cc4b34efa94ef87e1facd4ec443576f36
Camila Alm <camila.salmeidatech@hotmail.com>
2024-11-09 07:25:20 -0500

    background data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 // src/App.jsx
 import * as weatherService from './services/weatherService';
 import WeatherSearch from './components/WeatherSearch';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import WeatherDetails from './components/WeatherDetails';
-
-
 
 const App = () => {
   const [weather, setWeather] = useState({});
+
+useEffect(() => {
+const fetchDefaultData = async () => {
+  const data = await weatherService.show('New York');
+  const newWeatherState = {
+    location: data.location.name,
+    temperature: data.current.temp_f,
+    condition: data.current.condition.text,
+  };
+  setWeather(newWeatherState);
+  }
+  fetchDefaultData();
+}, []);
+
+
+
   const fetchData = async () => {
     console.log('1. The button has been clicked, triggering the fetchData function')
     const data = await weatherService.show('New York');
@@ -22,6 +36,7 @@ const App = () => {
   }
   
 return (
+ 
   <main>
     <h1>Weather API 🌦️</h1>
     <WeatherSearch fetchData={fetchData} />
